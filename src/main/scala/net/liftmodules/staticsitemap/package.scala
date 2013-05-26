fb373c5bf59799c5628329dd1989bb5b66cbf380
Jeff May <jeff.may@audaxhealth.com>
2013-05-26 11:24:01 -0400

    Added getAs helper methods. Closes #3

--- src/main/scala/net/liftmodules/staticsitemap/package.scala
+++ src/main/scala/net/liftmodules/staticsitemap/package.scala
@@ -2,4 +2,9 @@ package net.liftmodules
 
 package object staticsitemap {
 
+  class MissingRoutesException[Sitemap, Expected](implicit sitemap: Manifest[Sitemap], expected: Manifest[Expected])
+    extends Exception("Could not cast the SiteMap type %s as %s".format(
+      sitemap.runtimeClass.getSimpleName, expected.runtimeClass.getSimpleName)
+    )
+
 }
---
+++ src/test/scala/net/liftmodules/staticsitemap/TestSiteMaps.scala
@@ -0,0 +1,16 @@
+package net.liftmodules.staticsitemap
+
+import org.scalatest.{GivenWhenThen, FunSpec}
+import org.scalatest.matchers.ShouldMatchers
+import net.liftweb.mockweb.MockWeb._
+import net.liftweb.sitemap.SiteMap
+import net.liftweb.common.Full
+import net.liftweb.http.LiftSession
+
+class TestSiteMaps extends FunSpec
+with ShouldMatchers
+with GivenWhenThen {
+
+
+
+}
