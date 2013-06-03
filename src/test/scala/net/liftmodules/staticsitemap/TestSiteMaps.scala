3f34a4ce4337bf9fe654625e484e2a2de88f965f
Jeff May <jeff.may@audaxhealth.com>
2013-06-03 00:17:05 -0400

    Added some tests.

--- src/test/scala/net/liftmodules/staticsitemap/TestSiteMaps.scala
+++ src/test/scala/net/liftmodules/staticsitemap/TestSiteMaps.scala
@@ -2,9 +2,15 @@ package net.liftmodules.staticsitemap
 
 import net.liftweb.mockweb.WebSpec
 import net.liftweb.http.LiftRules
+import net.liftweb.sitemap.Loc.Template
+import net.liftweb.common.Full
 
 object SampleSiteMap extends StaticSiteMap {
-  val Index = @/(^)
+  val valid = <b>valid</b>
+  val validTemplate = Template(() => valid)
+
+  val Index = @/(^, validTemplate)
+  val Const = @/("const", validTemplate)
 }
 
 class RootIndexTest extends WebSpec(
@@ -13,9 +19,33 @@ class RootIndexTest extends WebSpec(
   }
 ) {
 
-  "SiteMap Root Index" should {
-    "Resolve the / url" withReqFor("/") in {
+  import SampleSiteMap._
+
+  "The SiteMap" should {
+    "- not resolve a non existant url" withReqFor("/empty") in {
+      _.location.isEmpty
+    }
+  }
+
+  "Root Index" should {
+
+    "- be a root menu" in {
+      Index.toRoute.toMenu.isRoot_?
+    }
+
+    "- resolve the / url" withReqFor("/") in {
       _.location.isDefined
     }
+
+    "- resolve the / url with Index menu" withReqFor("/") in {
+      req => Index.toRoute.toMenu.findLoc(req).isDefined
+    }
+
+    "- should resolve a template" withReqFor("/") in {
+      req => {
+        req.location.get.template must_== Full(valid)
+      }
+    }
+
   }
 }
